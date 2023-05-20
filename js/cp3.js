f795d2999bbdb8e52819f3257e4b48c4ad9603a6
kayqueln <rm550782@fiap.com.br>
2023-05-20 16:01:37 -0300

    implementação de imagens e validações

--- js/cp3.js
+++ js/cp3.js
@@ -1,6 +1,11 @@
 
-const form = document.querySelector('#form')
-const nome = document.getElementById('nome')
+const form = document.querySelector('#form');
+const nome = document.getElementById('nome');
+const sobrenome = document.getElementById('sobrenome');
+const email = document.getElementById('email')
+const senha = document.getElementById('senha')
+const senha2 = document.getElementById('senha-confirmacao')
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -10,7 +15,9 @@ form.addEventListener('submit', (e) => {
 
 
 function checkInputs(){
+    //Validação do campo nome
     const inputNome = nome.value.trim()
+    
     if (inputNome == ''){
         setErrorFor(nome, 'Preencha esse campo')
     } else if (inputNome.length < 5){
@@ -18,15 +25,67 @@ function checkInputs(){
     } else {
         setSucessFor(nome)
     }
+
+    //Validação do campo sobrenome
+    const sobrenomeInput = sobrenome.value.trim();
+
+    if (sobrenomeInput == ''){
+        setErrorFor(sobrenome, 'Preencha esse campo')
+    } else if (sobrenomeInput.length < 5){
+        setErrorFor(sobrenome, 'Esse campo deve ter o mínino de 5 caracteres')
+    } else {
+        setSucessFor(sobrenome)
+    }
+
+    //Validação do campo email
+    const emailInput = email.value.trim();
+
+    if (emailInput == ''){
+        setErrorFor(email, 'Preencha esse campo')
+    } else if (emailInput.length < 5){
+        setErrorFor(email, 'Esse campo deve ter o mínino de 5 caracteres')
+    } else {
+        setSucessFor(email)
+    }
+
+    //Validação do campo senha
+    const senhaInput = senha.value.trim();
+
+    if (senhaInput == ''){
+        setErrorFor(senha, 'Preencha esse campo')
+    } else if (senhaInput.length < 5){
+        setErrorFor(senha, 'Esse campo deve ter o mínino de 5 caracteres')
+    } else {
+        setSucessFor(senha)
+    }
+
+    //Validação do campo confirmação senha
+    const senha2Input = senha2.value.trim();
+
+    if (senha2Input == ''){
+        setErrorFor(senha2, 'Preencha esse campo')
+    } else if (senha2Input.length < 5){
+        setErrorFor(senha2, 'Esse campo deve ter o mínino de 5 caracteres')
+    } else {
+        setSucessFor(senha2)
+    }
+
 }
+
 
 function setErrorFor(input, message){
     const formControl = input.parentElement;
-    const label = formControl.querySelector('label')
+    var small = document.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error'
 }
+
+
 
 function setSucessFor(input){
     const formControl = input.parentElement;
-
-    formControl.classname = 'form-control success'
+    formControl.className = 'form-control success'
 }
+
